2b9e51f9a1b6fd78fc530a1f8120db7e5c7bc5c5
Morgan <Mharrisstoertz@gmail.com>
2023-01-12 15:03:16 -0500

    Initial Commit

---
+++ Playing Card/Playing_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Playing_CardApp.swift
+//  Playing Card
+//
+//  Created by Morgan Harris-Stoertz on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct Playing_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
